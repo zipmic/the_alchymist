423ae919837ffac5f66dac473b03d0d1cbb13ddd
Emil Kjaehr <emilk@Emils-MacBook-Pro-2.local>
2017-11-18 17:03:05 +0100

    Simple logging script, should definitely work on windows

---
+++ Assets/RFID_Tester/SimpleLog.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+
+public class SimpleLog : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("/dev/cu.usbmodem1421",9600);
+
+	int buttonState =0;
+
+	void Start(){
+		stream.Open();
+	}
+
+	void Update (){
+		string value = stream.Readline();
+		buttonState = int.Parse(value);
+	}
+
+	void OnGUI(){
+
+		string newString = "Connected: " + buttonState;
+		GUI.Label(new Rect(10,10,300,100), newString);
+	}
+}
